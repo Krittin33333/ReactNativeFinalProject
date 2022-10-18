c87cb9883aff86f1af5cf752b5715fb8aba96731
Krittin33333 <aomgg555@gmail.com>
2022-10-18 17:26:41 +0700

    Create SkillScreen.js

---
+++ screens/SkillScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SkillScreen = () => {
+  return (
+    <View>
+      <Text>SkillScreen</Text>
+    </View>
+  )
+}
+
+export default SkillScreen
+
+const styles = StyleSheet.create({})
